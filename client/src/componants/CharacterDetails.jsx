0eac58b26dfb2ba10c49f6b1b7ff6716ef15ffdc
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-05-30 14:33:03 -0700

    changed state to only allow one detail element

--- client/src/componants/CharacterDetails.jsx
+++ client/src/componants/CharacterDetails.jsx
@@ -4,10 +4,9 @@ import axios from 'axios';
 
 const CharacterDetails = () => {
     const [character, setCharacter] = useState(null);
+    const [activeElement, setActiveElement] = useState(null);
 
     let { charId } = useParams();
-
-    console.log(charId + " id")
 
     useEffect(() => {
         const fetchCharacterData = async () => {
@@ -26,17 +25,35 @@ const CharacterDetails = () => {
         return <div>Loading...</div>;
     }
 
+    const handleButtonClick = (element) => {
+
+        console.log(element);
+        setActiveElement(element === activeElement ? null : element);
+    };
+
     return (
         <div>
             <h1>Character Details</h1>
-            <h2>Name: {character.Information.Name}</h2>
-            <p>Pronouns: {character.Information.Pronouns}</p>
-            <p>Circle: {character.Information.Circle}</p>
-            <p>Style: {character.Information.Style}</p>
-            <p>Catalyst: {character.Information.Catalyst}</p>
-            <p>Question: {character.Information.Question}</p>
+            <div className="characterInformation bg-danger">
+                <button onClick={() => handleButtonClick('details')}>View Character details</button>
+                <button onClick={() => handleButtonClick('style')}>View Character Style</button>
+                <button onClick={() => handleButtonClick('catalyst')}>View Character Catalyst</button>
+                <button onClick={() => handleButtonClick('question')}>View Character Question</button>
 
+                {activeElement === 'details' && (
+                    <>
+                        <h2>Name: {character.Information.Name}</h2>
+                        <h2>Pronouns: {character.Information.Pronouns}</h2>
+                        <h2>Circle: {character.Information.Circle}</h2>
+                    </>
+                )}
+                {activeElement === 'style' && <h2>Style: {character.Information.Style}</h2>}
+                {activeElement === 'catalyst' && <h2>Catalyst: {character.Information.Catalyst}</h2>}
+                {activeElement === 'question' && <h2>Question: {character.Information.Question}</h2>}
+            </div>
             <h2>Stats</h2>
+
+            <div className="characterStats">
             <p>Nerve Move Value: {character.Stats.Nerve.move.value}</p>
             <p>Nerve Strike Value: {character.Stats.Nerve.strike.value}</p>
             <p>Nerve Control Value: {character.Stats.Nerve.control.value}</p>
@@ -51,16 +68,22 @@ const CharacterDetails = () => {
             <p>Intuition Focus Value: {character.Stats.Intuition.focus.value}</p>
             <p>Intuition Sense Value: {character.Stats.Intuition.sense.value}</p>
             <p>Intuition Drive Value: {character.Stats.Intuition.intuitionDrive.value}</p>
+            </div>
 
+            <div className='characterMarks'></div>
             <h2>Marks</h2>
             <p>Body: {character.Marks.body}</p>
             <p>Brain: {character.Marks.brain}</p>
             <p>Bleed: {character.Marks.bleed}</p>
+            <p>Relationships: {character.Relationships.join(', ')}</p>
+            <p>Scars: {character.Scars}</p>
 
+            <div className='characterAbilities'>
             <h2>Other Details</h2>
             <p>Role: {character.Role}</p>
             <p>Specialty: {character.Specialty}</p>
-            <p>Relationships: {character.Relationships.join(', ')}</p>
+            </div>
+
         </div>
     );
 };
